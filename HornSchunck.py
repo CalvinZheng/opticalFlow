18364e78dfe8481be148812b7284a02764516a08
emj29 <eemilja@Emil-Janulewiczs-MacBook-Pro.local>
2015-04-12 21:46:44 -0400

    :

--- HornSchunck.py
+++ HornSchunck.py
@@ -10,18 +10,25 @@ if __name__ == '__main__':
     
 cap = cv2.VideoCapture(fn)
 
+ret,prevframe=cap.read()
+prevgray=cv2.cvtColor(prevframe,cv2.COLOR_BGR2GRAY)
 while(cap.isOpened()):
     ret, frame = cap.read()
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     sobely = cv2.Sobel(gray,cv2.CV_64F,0,1,ksize=-1)
-    abs_sobely = np.absolute(sobely)
-    sobel_8u = np.uint8(abs_sobely)
-    gradient_y=np.gradient(gray)
+    time_grad=gray-prevgray
+    vy=-time_grad/sobely
+    #abs_sobely = np.absolute(sobely)
+    #sobel_8u = np.uint8(abs_sobely)
+    #gradient_y=np.gradient(gray)
 
-    cv2.imshow('frame',sobely)
-    print sobely[10:20,10:20]
+    #cv2.imshow('frame',sobely)
+    #cv2.imshow('frame',time_grad)
+    cv2.imshow('frame',vy)
+    #print sobely[10:20,10:20]
     if cv2.waitKey(1) & 0xFF == ord('q'):
          break
+    prevgray=gray
 
 cap.release()
 cv2.destroyAllWindows()
